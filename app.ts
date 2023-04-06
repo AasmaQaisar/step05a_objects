f2b2b413b20e8c82e88d39f6bb2becc67c6b512c
AasmaQaisar <125805604+AasmaQaisar@users.noreply.github.com>
2023-04-07 04:46:06 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,24 @@
+let teacher = {
+    name: "Janny",
+    experience: "10"
+}
+
+console.log(teacher.name);
+console.log(teacher["experience"]);
+
+
+// Type Declaration
+let student : {
+    name: string,
+    age: number
+}
+
+student = {
+    name: "Aasma",
+    age: 30
+}
+
+console.log(student["name"]);
+console.log(student.age);
+
+        
